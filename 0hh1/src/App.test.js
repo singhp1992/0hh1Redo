ca9d3e5f5bfbd436721dc3e4a2c3e12b9919ea72
Preeti <singhp1992@gmail.com>
2018-04-10 20:07:48 +0200

    added tests and created store using redux

--- 0hh1/src/App.test.js
+++ 0hh1/src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<App />', () => {
+  const app = shallow(<App />)
+
+  it('wraps everything in a div tag', () => {
+    expect(app).toHaveTagName('div')
+    expect(app).toHaveClassName('App')
+  })
+
+  it('contains a title', () => {
+    expect(app.find('h1.title')).toHaveText('0hh1')
+  })
+})
